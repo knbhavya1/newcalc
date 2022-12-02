2c4e3e2c94cfaa559dcc648c001f4f19b53777c2
KN Bhavya <bhavya.kn@datagrokr.com>
2022-12-02 18:46:46 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+class Calculator:
+
+  def __init__(self):
+    pass
+
+  def add(self, x1, x2):
+    return x1 + x2
+
+  def multiply(self, x1, x2):
+    return x1 * x2
+
+  def subtract(self, x1, x2):
+    return x1 - x2
+
